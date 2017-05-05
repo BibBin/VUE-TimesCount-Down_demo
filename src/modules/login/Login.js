c8fb15c471e16566024e0c7782bbd1480d908342
zhongwenbin <wbzhong@thetiger.com.cn>
2017-05-05 16:54:29 +0800

    父组件调用方法

--- src/modules/login/Login.js
+++ src/modules/login/Login.js
@@ -6,6 +6,7 @@ module.exports = {
   data() {
     return {
       codeUrl: '',
+      disabled: false,
       imgCodeShowFlag: false,
       typeFlag: '',
       data: {
@@ -21,13 +22,12 @@ module.exports = {
     }
   },
   methods: {
-    onSend(val){
-      //用户点击获取验证码，需要先输入图片验证码的时候，触发此事件
-      this.imgCodeShowFlag = true
-        this.requestId = uuid.v1().replace(/-/g, '')
-        this.codeUrl = `/ziwork/captcha?requestId=${this.requestId}`
-    },
-    
+      onSend(val){
+          this.imgCodeShowFlag = true//开启图片验证码dom显示
+          //相关操作用来调用图片验证码接口
+          this.requestId = uuid.v1().replace(/-/g, '')//获取随机数
+          this.codeUrl = `/ziwork/captcha?requestId=${this.requestId}`//尾增加随机数及时更新图片验证码
+      },
   },
   created() {
    
